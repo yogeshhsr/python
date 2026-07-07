e3bb3afcbf0bd8f5988131a1f68d5b5af40ff496
Pavankumar-1602 <cspavankumar123@gmail.com>
2026-07-07 15:31:17 +0530

    day 2

---
+++ operations.py
@@ -0,0 +1,30 @@
+# name = "i love mohan sir"
+# for char in name:
+#     print(char)
+
+########
+
+# count = 1
+# while count <= 5:
+#     print(f"sending mail {count}")
+#     count += 3
+
+######
+# for i in range(10):
+#     if i==5:
+#         break
+#     print(i)
+###
+# for i in range(10):
+#     if i==5:
+#         continue
+#     print(i)
+####
+student=[
+    "ram",
+    "sam",
+    "rama"
+]
+student.append("youme")
+student.remove("ram")
+print(student)
